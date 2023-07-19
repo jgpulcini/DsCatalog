9886380933464fa6a5426b19843500a889d8479a
jgpulcini <jgpulcini@gmail.com>
2023-07-19 11:34:51 -0300

    Criação de banco h2 e Category ( repository e service)

--- src/main/java/com/jgpulcini/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/jgpulcini/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,8 @@
 package com.jgpulcini.dscatalog.resources;
 
 import com.jgpulcini.dscatalog.entities.Category;
+import com.jgpulcini.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +14,12 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+    @Autowired
+    private CategoryService service;
 
     @GetMapping
     public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L,"Livros"));
-        list.add(new Category(2L, "Eletronicos"));
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
---
+++ src/main/java/com/jgpulcini/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.jgpulcini.dscatalog.services;
+
+import com.jgpulcini.dscatalog.entities.Category;
+import com.jgpulcini.dscatalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+}
