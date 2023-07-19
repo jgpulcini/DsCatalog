184865f93633f05934f85cd459890e629066ba84
jgpulcini <jgpulcini@gmail.com>
2023-07-19 11:53:43 -0300

    import.sql

--- src/main/java/com/jgpulcini/dscatalog/services/CategoryService.java
+++ src/main/java/com/jgpulcini/dscatalog/services/CategoryService.java
@@ -2,8 +2,10 @@ package com.jgpulcini.dscatalog.services;
 
 import com.jgpulcini.dscatalog.entities.Category;
 import com.jgpulcini.dscatalog.repositories.CategoryRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,9 +14,10 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository repository;
-
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return repository.findAll();
     }
+
 
 }
